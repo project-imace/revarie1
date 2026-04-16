7892efc4ee2d5415f13d645860aae45b9a9e8733
Ariartry <ariartry@proton.me>
2026-04-16 06:46:07 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -14,6 +14,14 @@ const nextConfig = {
   experimental: {
     optimizeCss: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/vault/:path*',
+        destination: 'https://revarie-vault-api.revarie-lm-v1-study-1-vault.workers.dev/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
